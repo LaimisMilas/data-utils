7af980197ad975a425deac57e89c7f34dcad92b5
Laimonas <laimonas.milasius@latgrupe.lt>
2022-04-08 16:52:27 +0300

    git ignor

--- src/main/java/data/utils/VisualWebPageView.java
+++ src/main/java/data/utils/VisualWebPageView.java
@@ -16,21 +16,12 @@ public class VisualWebPageView {
 	
 	private static WebDriver driver;
 	
-	public static void initFireFox() {
-
-		driver = new FirefoxDriver();
-
-	}
-	
-	
-	public static void doJob() {
-		
-		List<HashMap<String, Object>> companys = getCompanys();
-
-		for (int a = 0; a < companys.size(); a++) {
-
-			HashMap<String, Object> company = companys.get(a);
-
+	// pasiims sarasa kompaniju ir ciklu per visas atidarines ju puslapius.
+	// cia yra rekvizitu puslapis kur galima nuskaityti detalesne kompanijos informacija
+	public static void multipleVisits() {
+		List<HashMap<String, Object>> companies = getCompanies();
+		for (int a = 0; a < companies.size(); a++) {
+			HashMap<String, Object> company = companies.get(a);
 			Integer id = (Integer) company.get("id");
 			String companyCode = (String) company.get("company_code");
 			String email = (String) company.get("email");
@@ -38,65 +29,47 @@ public class VisualWebPageView {
 			String webSiteUrl = (String) company.get("web_site_url");
 			Integer domainStatus = (Integer) company.get("domain_status");
 			String domainProtocol = (String) company.get("domain_protocol");
-			
 			if(HTMLFetcher.ignorWords.contains(webSiteUrl)) {
 				continue;
 			}
-				
 			driver.get(domainProtocol + "://" + webSiteUrl);
-			
 			System.out.println(a + " domainStatus " + domainStatus + " " + domainProtocol + "://" + webSiteUrl);
-			
 			PageUtils.scrollPageDown(1, 300, 1000, driver);
-			
 			Utils.sleepThread(4000);
 		}
 	}
 
-
-	private static List<HashMap<String, Object>> getCompanys() {
-		
-		List<HashMap<String, Object>>  companys = new ArrayList<HashMap<String, Object>>(50);
-		
+	// Grazina companiju sarasa is BD lenteles company_info
+	// tik nera aisku kaip selectina, atrodo yra atskira domenu lentele,
+	// pagal tai iesko url'o kas ner 'web_site_url', cia tiktu contains
+	private static List<HashMap<String, Object>> getCompanies() {
+		List<HashMap<String, Object>>  companies = new ArrayList<HashMap<String, Object>>(50);
+		//yra lentele kuri saugo domain_name laukai: domain_name, status, note, protocol, company_code
 		List<HashMap<String, Object>>  domains = DomainNameDAO.selectAllByStatus(2);
-
 		for (int a = 0; a < domains.size(); a++) {
-			
 			HashMap<String, Object> domain = domains.get(a);
-			
 			String domainName = (String) domain.get("domain_name");
-			
-			List<HashMap<String, Object>>  rCompanys = CompanyInfoDAO.getByWebSiteUrl(domainName);
-			
-			if(rCompanys != null && rCompanys.size() > 0) {
-				
-				HashMap<String, Object> rCompany = rCompanys.get(0);
-				
+			// ten net nera LIKI ar kasnors panasaus yra =. nebent lentoje saugomas visas url'as.
+			List<HashMap<String, Object>>  rCompanies = CompanyInfoDAO.getByWebSiteUrl(domainName);
+			if(rCompanies != null && rCompanies.size() > 0) {
+				HashMap<String, Object> rCompany = rCompanies.get(0);
 				rCompany.put("domain_name",(String) domain.get("domain_name"));
 				rCompany.put("domain_status",(String) domain.get("status"));
 				rCompany.put("domain_protocol",(String) domain.get("protocol"));
-				
-				companys.add(rCompany);
-				
+				companies.add(rCompany);
 			}
 		}
-		
-		return companys;
+		return companies;
 	}
 	
 	public static void main(String[] args) {
-		
-		String geckoDriver = "/home/laimonas/java/geckodriver-v0.24.0-linux64/geckodriver";
-		System.setProperty("webdriver.gecko.driver", geckoDriver);
-		
-	    VisualWebPageView.initFireFox();
-		VisualWebPageView.doJob();
+		SeleniumUtils.setGeckoDriver();
+		driver = SeleniumUtils.initFireFox("");
+		VisualWebPageView.multipleVisits();
 		//VisualWebPageView.singleVisit("http://reikalingaaukle.lt");
 	}
 
-
 	private static void singleVisit(String webSiteUrl) {
-		
 		if(!HTMLFetcher.ignorWords.contains(webSiteUrl)) {
 			driver.get(webSiteUrl);
 			System.out.println(webSiteUrl);
